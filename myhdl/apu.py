f222411582382c0a853bbf773970441af27932ee
strfry <strfry@c5a0b408-f557-4aed-80d3-2ab94eae09d5>
2013-03-29 03:07:30 +0000

    Envelope stuff

--- myhdl/apu.py
+++ myhdl/apu.py
@@ -4,10 +4,93 @@ NES_CLK_period = 46 * 12
 #  constant AC97_CLK_period : time := 20.833333333333332 us; -- 48 kHz
 #  constant CLK_period : time := 46.560848137510206 ns; -- 21.477272 MhZ
 
+def APU_FrameCounter(
+		CLK,
+		APU_CE,
+
+		Mode,
+		InterruptInhibit,
+
+		QuarterFrameOut,
+		HalfFrameOut,
+		InterruptFlagOut
+		):
+
+	timer = Signal(intbv())
+
+	@always(CLK.posedge)
+	def logic():
+		QuarterFrameOut.next = False
+		HalfFrameOut.next = False
+
+		if APU_CE:
+			timer.next = timer + 1
+
+			if timer == 3728:
+				QuarterFrameOut.next = True
+			elif timer == 7456:
+				HalfFrameOut.next = True
+				QuarterFrameOut.next = True
+			elif timer == 11186:
+				QuarterFrameOut.next = True
+			elif not Mode and timer == 14914:
+				HalfFrameOut.next = True
+				QuarterFrameOut.next = True
+				timer.next = 0
+			elif Mode and timer == 18640:
+				HalfFrameOut.next = True
+				QuarterFrameOut.next = True
+				timer.next = 0
+
+	return logic
+
+
+def APU_Envelope(
+		CLK,
+		HalfFrame_CE,
+
+		StartFlag,
+		LoopFlag,
+		ConstantFlag,
+		
+		LengthCounterLoad,
+		VolumeDecay,
+
+		VolumeOut
+		):
+
+	timer = Signal(intbv()[4:0])
+
+	@always(CLK.posedge)
+	def logic():
+		if HalfFrame_CE:
+			if StartFlag:
+				print "Start Envelope"
+				StartFlag.next = False
+				VolumeOut.next = 15
+				timer.next = VolumeDecay
+			else:
+				if timer == 0:
+					timer.next = VolumeDecay
+					if VolumeOut != 0:
+						VolumeOut.next = VolumeOut - 1
+					else:
+						if LoopFlag:
+							VolumeOut.next = 15						
+				else:
+					timer.next = timer - 1
+
+	return logic
+
+
+
+
 def APU_Pulse(
 		CLK,
 		APU_CE,
 
+		EnvelopeIn,
+		DutyCycleMode,
 		TimerLoad,
 
 		PCM_OUT
@@ -21,10 +104,9 @@ def APU_Pulse(
 	@always(CLK.posedge)
 	def logic():
 		if APU_CE:
-			#print timer
 			if timer == 0:
 				sequencer.next = concat(sequencer[0], sequencer[8:1])
-				PCM_OUT.next = 0x0f if sequencer[0] else 0x00
+				PCM_OUT.next = EnvelopeIn if sequencer[0] else 0x00
 				timer.next = TimerLoad
 			else:
 				timer.next = timer - 1
@@ -50,10 +132,39 @@ def APU_Triangle(
 			if timer == 0:
 				sequencer.next = (sequencer + 1) % 32
 				PCM_OUT.next = lut[sequencer]
-				#timer.next = TimerLoad
-				timer.next = 0x0fd
+				timer.next = TimerLoad
+				#timer.next = 0x0fd
 			else:
 				timer.next = timer - 1
+
+	return logic
+
+def APU_Noise(
+		CLK,
+		APU_CE,
+
+		EnvelopeIn,
+
+		TimerPeriod,
+		Mode,
+
+
+		PCM_OUT
+		):
+
+	lut = [4, 8, 16, 32, 64, 96, 128, 160, 202, 254, 380, 508, 762, 1016, 2034, 4068]
+	lfsr = Signal(intbv("111111111111111")[15:0])
+	timer = Signal(intbv())
+
+	@always(CLK.posedge)
+	def logic():
+		if timer == 0:
+			fb_bit = lfsr[0] ^ (lfsr[6] if Mode else lfsr[1])
+			lfsr.next = concat(fb_bit, lfsr[15:1])
+			PCM_OUT.next = EnvelopeIn if lfsr[0] else 0x00
+			timer.next = lut[TimerPeriod]
+		else:
+			timer.next = timer - 1
 
 	return logic
 
--- myhdl/apu_tb.py
+++ myhdl/apu_tb.py
@@ -99,6 +99,13 @@ def TestBench():
 	APU_CE = Signal(False)
 	PCM = Signal(intbv()[8:])
 
+	FrameCounterMode = Signal(False)
+	InterruptInhibit = Signal(False)
+
+	QuarterFrame_CE = Signal(False)
+	HalfFrame_CE = Signal(False)
+	InterruptFlag = Signal(False)
+
 	Pulse1Timer = Signal(intbv())
 	Pulse2Timer = Signal(intbv())
 	TriangleTimer = Signal(intbv())
@@ -106,16 +113,120 @@ def TestBench():
 	Pulse1PCM = Signal(intbv()[4:])
 	Pulse2PCM = Signal(intbv()[4:])
 	TrianglePCM = Signal(intbv()[4:])
+	NoisePCM = Signal(intbv()[4:])
+
+	
+	Pulse1Timer = Signal(intbv())
+	Pulse1Duty = Signal(intbv()[2:0])
+	Pulse1StartFlag = Signal(False)
+	Pulse1LoopFlag = Signal(False)
+	Pulse1ConstantFlag = Signal(False)
+	Pulse1Mode = Signal(False)
+	Pulse1Length = Signal(intbv())
+	Pulse1Volume = Signal(intbv()[4:0])
+	Pulse1EnvelopeVolume = Signal(intbv()[4:0])
+
+	Pulse1Envelope = Signal(intbv())
+
+	def pulse1_writehook(address, data):
+		data = intbv(data)[8:0]
+		if address == 0x4000:
+			Pulse1Duty.next = data[8:6]
+			Pulse1LoopFlag.next = data[5]
+			Pulse1ConstantFlag.next = data[4]
+			Pulse1Volume.next = data[4:0]
+		elif address == 0x4002:
+			Pulse1Timer.next[8:0] = data
+			Pulse1Mode.next = data[7]
+		elif address == 0x4003:
+			Pulse1Length.next = data[8:3]
+			Pulse1Timer.next[11:8] = data[3:0]
+			Pulse1StartFlag.next = True
+			print "Start Pulse1 Envelope"
+
+	mem.subscribe_to_write(range(0x4000, 0x4004), pulse1_writehook)
+
+	
+	Pulse2Timer = Signal(intbv())
+	Pulse2Duty = Signal(intbv()[2:0])
+	Pulse2StartFlag = Signal(False)
+	Pulse2LoopFlag = Signal(False)
+	Pulse2ConstantFlag = Signal(False)
+	Pulse2Mode = Signal(False)
+	Pulse2Length = Signal(intbv())
+	Pulse2Volume = Signal(intbv()[4:0])
+	Pulse2EnvelopeVolume = Signal(intbv()[4:0])
+
+	Pulse2Envelope = Signal(intbv())
+
+	def pulse2_writehook(address, data):
+		data = intbv(data)[8:0]
+		if address == 0x4004:
+			Pulse2Duty.next = data[8:6]
+			Pulse2LoopFlag.next = data[5]
+			Pulse2ConstantFlag.next = data[4]
+			Pulse2Volume.next = data[4:0]
+		elif address == 0x4006:
+			Pulse2Timer.next[8:0] = data
+			Pulse2Mode.next = data[7]
+		elif address == 0x4007:
+			Pulse2Length.next = data[8:3]
+			Pulse2Timer.next[11:8] = data[3:0]
+			Pulse2StartFlag.next = True
+			print "Start Pulse2 Envelope"
+
+	mem.subscribe_to_write(range(0x4004, 0x4008), pulse2_writehook)
+
+
+	NoiseTimer = Signal(intbv())
+	NoiseStartFlag = Signal(False)
+	NoiseLoopFlag = Signal(False)
+	NoiseConstantFlag = Signal(False)
+	NoiseMode = Signal(False)
+	NoiseLength = Signal(intbv())
+	NoiseVolume = Signal(intbv()[4:0])
+	NoiseEnvelopeVolume = Signal(intbv()[4:0])
+	
+	def noise_writehook(address, data):
+		print "Noise writehook"
+		data = intbv(data)
+		if address == 0x400c:
+			NoiseLoopFlag.next = data[5]
+			NoiseConstantFlag.next = data[4]
+			NoiseVolume.next = data[4:0]
+		elif address == 0x400e:
+			NoiseMode.next = data[7]
+			NoiseTimer.next = data[4:0]
+		elif address == 0x400f:
+			NoiseLength.next = data[8:3]
+			NoiseStartFlag.next = True
+			print "Noise Channel started"
+
+	mem.subscribe_to_write(range(0x400c, 0x4010), noise_writehook)
+
 
 	@always_comb
 	def comb():
-		PCM.next = Pulse1PCM + Pulse2PCM + TrianglePCM
+		PCM.next = Pulse1PCM + Pulse2PCM + NoisePCM
 		#PCM.next = TrianglePCM
 
 	nes_clk_gen = clk_gen(NES_CLK, NES_CLK_period)
-	pulse1 = APU_Pulse(NES_CLK, APU_CE, Pulse1Timer, Pulse1PCM)
-	pulse2 = APU_Pulse(NES_CLK, APU_CE, Pulse2Timer, Pulse2PCM)
+
+	frameCounter = APU_FrameCounter(NES_CLK, APU_CE, FrameCounterMode, InterruptInhibit, QuarterFrame_CE, HalfFrame_CE, InterruptFlag)
+
+	pulse1Envelope = APU_Envelope(NES_CLK, HalfFrame_CE, Pulse1StartFlag, Pulse1LoopFlag, Pulse1ConstantFlag, Pulse1Length, Pulse1Volume, Pulse1EnvelopeVolume)
+	pulse2Envelope = APU_Envelope(NES_CLK, HalfFrame_CE, Pulse2StartFlag, Pulse2LoopFlag, Pulse2ConstantFlag, Pulse2Length, Pulse2Volume, Pulse2EnvelopeVolume)
+	noiseEnvelope = APU_Envelope(NES_CLK, HalfFrame_CE, NoiseStartFlag, NoiseLoopFlag, NoiseConstantFlag, NoiseLength, NoiseVolume, NoiseEnvelopeVolume)
+
+	pulse1 = APU_Pulse(NES_CLK, APU_CE, Pulse1EnvelopeVolume, Pulse1Duty, Pulse1Timer, Pulse1PCM)
+	pulse2 = APU_Pulse(NES_CLK, APU_CE, Pulse2EnvelopeVolume, Pulse2Duty, Pulse2Timer, Pulse2PCM)
 	triangle = APU_Triangle(NES_CLK, APU_CE, TriangleTimer, TrianglePCM)
+	noise = APU_Noise(NES_CLK, APU_CE, NoiseEnvelopeVolume, NoiseTimer, NoiseMode, NoisePCM)
+
+
+	# noiseEnvelope(NES_CLK, )
+
+
 	ac97 = AC97_WavWriter(AC97_CLK, PCM, "pulse.wav")
 
 	num_cycles = Signal(cpu.processorCycles)
@@ -142,25 +253,19 @@ def TestBench():
 			num_cycles.next = num_cycles + 1
 			if num_cycles == cpu.processorCycles:
 				cpu.step()
-				
-				timer_low = intbv(mem[0x4002])[8:0]
-				timer_high = intbv(mem[0x4003])[3:0]
 
-				Pulse1Timer.next = concat(timer_high, timer_low)
-
-				timer_low = intbv(mem[0x4006])[8:0]
-				timer_high = intbv(mem[0x4007])[3:0]
-
-				Pulse2Timer.next = concat(timer_high, timer_low)
-				
+				FrameCounterMode.next = intbv(mem[0x4017])[7]
+				InterruptInhibit.next = intbv(mem[0x4017])[6]
+								
 				timer_low = intbv(mem[0x400a])[8:0]
 				timer_high = intbv(mem[0x400b])[3:0]
 
 				TriangleTimer.next = concat(timer_high, timer_low)
 
+
 	#foo = traceSignals(APU_Pulse, NES_CLK, APU_CE, PCM)
 
-	return pulse1, pulse2, triangle, ac97, nes_clk_gen, ce, comb
+	return pulse1, pulse2, triangle, noise, ac97, nes_clk_gen, ce, comb, frameCounter, noiseEnvelope, pulse1Envelope, pulse2Envelope
 
 Simulation(TestBench()).run()
 
