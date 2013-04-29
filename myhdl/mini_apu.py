eb90c3caca0bf3a5bc54dd3a26380994d67b6d16
strfry <strfry@c5a0b408-f557-4aed-80d3-2ab94eae09d5>
2013-04-29 00:03:10 +0000

    Wrote some description about mini testbench and fixed it somewhat

--- myhdl/mini_apu.py
+++ myhdl/mini_apu.py
@@ -3,29 +3,28 @@ from myhdl import *
 from cpu_bus import CPU_Bus
 
 
-def APU_Main(cpu_bus, PCM_out):
-#	TimerLoad = 0x0FD
-	TimerLoad = Signal(intbv())
+def APU_Main(
+	CLK, PHI2, RW10, Address, Data_write,
+	PCM_out):
+
+	TimerLoad = Signal(intbv()[11:0])
 
 	sequencer = Signal(intbv("00001111"))
-	timer = Signal(intbv(0))
+	timer = Signal(intbv()[11:0])
 
-	@always(cpu_bus.CLK.posedge)
+	@always(CLK.posedge)
 	def logic():
-		if cpu_bus.PHI2 and cpu_bus.RW10 == 0:
-			if cpu_bus.Address == 0x4002:
-				TimerLoad.next[8:] = cpu_bus.Data_out
-			elif cpu_bus.Address == 0x4003:
-				TimerLoad.next[11:8] = cpu_bus.Data_out[3:0]
-
-
-		if cpu_bus.PHI2:
+		if PHI2 and RW10 == 0:
+			if Address == 0x4002:
+				TimerLoad.next[8:] = Data_write
+			elif Address == 0x4003:
+				TimerLoad.next[11:8] = Data_write[3:0]
+		if PHI2:
 			if timer == 0:
 				sequencer.next = concat(sequencer[0], sequencer[8:1])
 				PCM_out.next = 0xf if sequencer[0] else 0x00
+				#print sequencer
 				timer.next = TimerLoad
-#				timer.next = 0x7f1
 			else:
 				timer.next = timer - 1
-
 	return logic
--- myhdl/mini_apu_tb.py
+++ myhdl/mini_apu_tb.py
@@ -1,9 +1,14 @@
+"""
+	Minimal Testbench with an APU_Pulse unit that is sweeped via CPU_Bus writes
+	The generated sweep can be used to implement a FIR filter in AC97_Writer
+"""
+
 from myhdl import *
 from mini_apu import *
 from cpu_bus import CPU_Bus
 
 
-NES_CLK_period = 46
+NES_CLK_period = 46 # ns
 #  constant AC97_CLK_period : time := 20.833333333333332 us; -- 48 kHz
 #  constant CLK_period : time := 46.560848137510206 ns; -- 21.477272 MhZ
 
@@ -29,7 +34,7 @@ def AC97_WavWriter(PCM, filename):
 
         @always(CLK.posedge)
         def writeSample():
-                outfile.writeframes(chr(int(PCM) * 4))
+                outfile.writeframes(chr(int(PCM * 16)))
 
         return ac97_clk_gen, writeSample
 
@@ -38,7 +43,6 @@ def APU_TB():
 	PCM = Signal(intbv()[8:])
 	cpu_bus = CPU_Bus()
 
-
         CLK_cnt = Signal(intbv())
 	@always(cpu_bus.CLK.posedge)
 	def clk_cnt():
@@ -46,20 +50,47 @@ def APU_TB():
 
         @always_comb
         def ce():
-                cpu_bus.PHI2 = CLK_cnt == 5
+                cpu_bus.PHI2.next = CLK_cnt == 5
+		cpu_bus.PHI2.next = True
 
-	clk_gen = CLK_Gen(cpu_bus.CLK, NES_CLK_period)
+	clk_gen = CLK_Gen(cpu_bus.CLK, NES_CLK_period * 12)
 	ac97 = AC97_WavWriter(PCM, "mini.wav")
-	apu = APU_Main(cpu_bus, PCM)
+	apu = APU_Main(cpu_bus.CLK, cpu_bus.PHI2, cpu_bus.RW10,
+		cpu_bus.Address, cpu_bus.Data_write, PCM)
 
 
-	bar = Signal(intbv())
-	@always(delay(10000000))
-	def foo():
-		bar.next = bar + 5
-		cpu_bus.fake_write(0x4002, bar)
-		print "increase"
+	sweepTimer = Signal(intbv()[11:0])
+
+	@instance
+	def sweep():
+		for i in range(0x7ff):
+			sweepTimer = intbv(i)
+			cpu_bus.fake_write(0x4002, sweepTimer[8:0])
+			cpu_bus.fake_write(0x4003, sweepTimer[11:8])
+		
+			# Print new frequency
+			pulse_period = (sweepTimer + 1) * NES_CLK_period * 12 * 8 *  10**-9
+			frequency = 1.0 / (pulse_period * 2)
+		
+			print "Increasing period to %d cycles, (%f Hz)" % (sweepTimer, frequency)
+
+			# Walk through lower period ranges faster to emulate an exponential sweep
+			import math
+			octave = int(math.log(frequency, 2))
+
+			yield delay(100000000 / (i+1))
+		raise StopSimulation
+
+		
 
 	return instances(), cpu_bus.instances
 
+def convert():
+	cpu_bus = CPU_Bus()
+	pcm = Signal(intbv()[8])
+	toVHDL(APU_Main, cpu_bus.CLK, cpu_bus.PHI2, cpu_bus.RW10,
+		cpu_bus.Address, cpu_bus.Data_write,
+		pcm)
+
+#convert()
 Simulation(APU_TB()).run()
