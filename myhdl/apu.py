88aa2bcd3a00d30b4803e5860a39327298aa4400
strfry <strfry@c5a0b408-f557-4aed-80d3-2ab94eae09d5>
2013-04-30 00:57:11 +0000

    Noise Channel

--- myhdl/apu.py
+++ myhdl/apu.py
@@ -23,16 +23,19 @@ def APU_Main(
 
 	Pulse1_CS = Signal(False)
 	Pulse2_CS = Signal(False)
+	Noise_CS = Signal(False)
 
 	HalfFrame_CE = Signal(False)
 	QuarterFrame_CE = Signal(False)
 
 	PCM_pulse1 = Signal(intbv())
 	PCM_pulse2 = Signal(intbv())
+	PCM_noise = Signal(intbv())
 
 	frameCounter = APU_FrameCounter(CLK, APU_CE, RW10, Address, Data_write, HalfFrame_CE, QuarterFrame_CE, Interrupt)
 	pulse1 = APU_Pulse(CLK, APU_CE, RW10, Address, Data_write, Pulse1_CS, HalfFrame_CE, QuarterFrame_CE, PCM_pulse1)
 	pulse2 = APU_Pulse(CLK, APU_CE, RW10, Address, Data_write, Pulse2_CS, HalfFrame_CE, QuarterFrame_CE, PCM_pulse2)
+	noise = APU_Noise(CLK, APU_CE, RW10, Address, Data_write, Noise_CS, HalfFrame_CE, QuarterFrame_CE, PCM_noise)
 
 	@always(CLK.posedge)
 	def ce():
@@ -43,9 +46,10 @@ def APU_Main(
 	def chipselect():
 		Pulse1_CS.next = 0x4000 <= Address and Address < 0x4004
 		Pulse2_CS.next = 0x4004 <= Address and Address < 0x4008
+		Noise_CS.next = 0x400C <= Address and Address < 0x4010
 		APU_CE.next = PHI1_CE and APU_CE_cnt
 
-		PCM_out.next = PCM_pulse1 + PCM_pulse2
+		PCM_out.next = PCM_pulse1 + PCM_pulse2 + PCM_noise
 
 	return instances()
 
@@ -225,6 +229,94 @@ def APU_Pulse(
 				timer.next = timer - 1
 	return instances()
 
+def LengthCounter2(
+	CLK, HalfFrame_CE,
+	LengthCounterHalt, LengthCounterLoad, LengthCounterLoadFlag,
+	Enable_out
+	):
+
+	LengthCounter = Signal(intbv()[8:0])
+
+	# Lookup Table for Length Counter values
+	LC_lut = [
+		10, 254, 20,  2, 40,  4, 80,  6,
+		160,  8, 60, 10, 14, 12, 26, 14,
+		12, 16, 24, 18, 48, 20, 96, 22,
+		192, 24, 72, 26, 16, 28, 32, 30
+	]
+
+	@always(CLK.posedge)
+	def logic():
+		if HalfFrame_CE:
+			if LengthCounter and not LengthCounterHalt:
+				print LengthCounter
+				LengthCounter.next = LengthCounter - 1
+		
+		if LengthCounterLoadFlag:
+			LengthCounter.next = LC_lut[LengthCounterLoad]
+
+	@always_comb
+	def comb():
+		Enable_out.next = LengthCounter > 0
+
+	return instances()
+
+def APU_Noise(
+	CLK, APU_CE, RW10, Address, Data_write,
+	ChipSelect, HalfFrame_CE, QuarterFrame_CE,
+	PCM_out):
+
+	lut = [4, 8, 16, 32, 64, 96, 128, 160, 202, 254, 380, 508, 762, 1016, 2034, 4068]
+	lfsr = Signal(intbv("111111111111111")[15:0])
+	timer = Signal(intbv())
+
+	TimerLoad = Signal(intbv())
+	LFSRMode = Signal(False)
+
+	LengthCounterHalt = Signal(False)
+	LengthCounterLoadFlag = Signal(False)
+	LengthCounterLoad = Signal(intbv())
+
+	lengthCounterEnable = Signal(False)
+
+	DutyCycle = Signal(intbv()[2:0])
+
+	EnvelopeDecay = Signal(intbv()[4:0])
+	EnvelopeConstantFlag = Signal(False)
+	EnvelopeStartFlag = Signal(False)
+	EnvelopeVolume = Signal(intbv()[4:0])
+
+	lengthCounter = LengthCounter2(CLK, HalfFrame_CE, LengthCounterHalt, LengthCounterLoad, LengthCounterLoadFlag, lengthCounterEnable)
+	envelope = APU_Envelope(CLK, QuarterFrame_CE, EnvelopeStartFlag, Signal(False), EnvelopeConstantFlag,
+                EnvelopeDecay, EnvelopeVolume)	
+
+
+	@always(CLK.posedge)
+	def logic():
+		LengthCounterLoadFlag.next = False
+		if APU_CE and RW10 == 0 and ChipSelect:
+			if Address[2:0] == 0x0:
+				LengthCounterHalt.next = Data_write[5]
+				EnvelopeConstantFlag.next = Data_write[4]
+				EnvelopeDecay.next = Data_write[4:0]
+				print "Noise Envelope config: Constant:", EnvelopeConstantFlag.next, "Decay: ", EnvelopeDecay.next
+			elif Address[2:0] == 0x2:
+				LFSRMode.next = Data_write[7]
+				TimerLoad.next[4:0] = Data_write[4:0]
+			elif Address[2:0] == 0x3:
+				EnvelopeStartFlag.next = True
+				LengthCounterLoad.next = Data_write[8:3]
+				LengthCounterLoadFlag.next = True
+		if APU_CE:
+			if timer == 0:
+				fb_bit = lfsr[0] ^ (lfsr[6] if LFSRMode else lfsr[1])
+				lfsr.next = concat(fb_bit, lfsr[15:1])
+				PCM_out.next = EnvelopeVolume if lfsr[0] and lengthCounterEnable else 0x00
+				timer.next = lut[TimerLoad]
+			else:
+				timer.next = timer - 1
+	return instances()
+
 
 def APU_Triangle(
 		CLK,
@@ -249,35 +341,4 @@ def APU_Triangle(
 				timer.next = 0x0fd
 			else:
 				timer.next = timer - 1
-
-	return logic
-
-def APU_Noise(
-		CLK,
-		APU_CE,
-
-		EnvelopeIn,
-
-		TimerPeriod,
-		Mode,
-
-
-		PCM_OUT
-		):
-
-	lut = [4, 8, 16, 32, 64, 96, 128, 160, 202, 254, 380, 508, 762, 1016, 2034, 4068]
-	lfsr = Signal(intbv("111111111111111")[15:0])
-	timer = Signal(intbv())
-
-	@always(CLK.posedge)
-	def logic():
-		if timer == 0:
-			fb_bit = lfsr[0] ^ (lfsr[6] if Mode else lfsr[1])
-			lfsr.next = concat(fb_bit, lfsr[15:1])
-			PCM_OUT.next = EnvelopeIn if lfsr[0] else 0x00
-			timer.next = lut[TimerPeriod]
-		else:
-			timer.next = timer - 1
-
-	return logic
-
+	return instances()
